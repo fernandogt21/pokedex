5efa9d677b3f35af8aec96a6442b9005d9d03964
fernandogt <fernandogt21@hotmail.com>
2022-11-16 01:10:10 +0100

    Primer Commit

---
+++ src/app.module.ts
@@ -0,0 +1,37 @@
+import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { Mongoose } from 'mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
+
+
+@Module({
+  imports: [
+
+    ConfigModule.forRoot(),
+
+    ConfigModule.forRoot({
+      load: [ EnvConfiguration ],
+      validationSchema: JoiValidationSchema,
+    }),
+
+    ServeStaticModule.forRoot({
+    rootPath: join(__dirname,'..','public'),
+    }),
+
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+
+    PokemonModule,
+
+    CommonModule,
+
+    SeedModule  
+  ], 
+})
+export class AppModule {}
